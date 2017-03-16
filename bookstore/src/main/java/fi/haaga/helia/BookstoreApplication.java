1dea8ccd8b90841d6da79c5e0b91b9a7ce9a483b
Pieviläinen Niko <a1503096@myy.haaga-helia.fi>
2017-03-16 15:32:22 +0200

    first commit

---
+++ bookstore/src/main/java/fi/haaga/helia/BookstoreApplication.java
@@ -0,0 +1,28 @@
+package fi.haaga.helia;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haaga.helia.domain.Book;
+
+import fi.haaga.helia.repository.BookRepository;
+
+
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book book = new Book();
+			repository.save(new Book("Franz Kafka","0553213695","The Metamorphosis",1912));
+		};
+	}
+}
